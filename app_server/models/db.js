c15138568027e661d53280c4ba419e1dac71a908
thsu3131 <thsu3131@gmail.com>
2017-07-25 16:08:54 -0500

    Connecting the Express application to MongoDB using Mongoose

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,1 +1,66 @@
+/*Define database conection string and use it to open Mongoose connection */
 var mongoose = require('mongoose');
+var gracefulShutdown;
+var dbURI = 'mongodb://localhost/coffee'
+mongoose.connect(dbURI);
+
+
+/*Listen for Mongoose connection events and output statuses to console*/
+mongoose.connection.on('connected', function(){
+    console.log('Mongoose connected to' + dbURI);
+});
+
+
+mongoose.connection.on('error', function(err){
+    console.log('Mongoose connection error' + err);
+});
+
+mongoose.connection.on('disconnected', function(){
+    console.log('Mongoose disconnected');
+
+});
+
+/*Reuseable function to close Mongoose connection*/
+
+gracefulShutdown = function(msg, callback){
+    mongoose.connection.close(function(){
+    	console.log('Mongoose disconnected through'+msg);
+    	callback();
+    });
+
+};
+
+/*Listen to Node processes for termination or restart signals and call gracefulShutdown function when
+appropriate, pass a continuation callback*/
+
+/*If the node process ends, close the Mongoose connection*/
+
+/*For nodemon  retarts*/
+process.once('SIGUSR2',  function(){
+  gracefulShutdown('nodemon restart', function(){
+  	process.kill(process.pid, 'SIGUSR2');
+
+  });
+});
+
+/*For app termination*/
+
+/*If the Node process ends, close the Mongoose connection*/
+process.on('SIGINT',  function(){
+	gracefulShutdown('app termination', function(){
+		process.exit(0);
+
+	});
+
+});
+
+/*For Heroku app termination*/
+process.on('SIGINT', function(){
+    gracefulShutdown('Heroku app shutdown', function(){
+    	process.exit(0);
+
+    });
+
+});
+
+/*If the node process ends, close the Mongoose connection*/
