487feb44a79d9e7b0b85aaf122a696a620ea5671
thsu3131 <thsu3131@gmail.com>
2017-09-07 12:09:50 -0500

    Heroku deploy 9/7/2017 12:09

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -19,14 +19,14 @@ var formatDistance = function(distance){
 var renderHomepage = function(req, res, responseBody){
 	var message;
 	
-	if(!(responseBody instanceof Array)){
+	/*if(!(responseBody instanceof Array)){
 		message = "API lookup errorrrrrr";
 		responseBody = [];
 	}else{
 		if(!responseBody.length){
 			message = "No place found nearby";
 		}
-	}
+	}*/
 	res.render('locations-list',{
 		title: 'COFFEE NEAR ME - COFFEE NEAR ME helps you find the best places for a coffee in Wichita!',
 		pageHeader:{
